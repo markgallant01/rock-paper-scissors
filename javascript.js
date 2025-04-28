64dca623cadfaa726206242683b5745714876c06
markgallant01 <markgallant01@gmail.com>
2025-04-28 01:14:11 -0400

    implement playGame function

--- javascript.js
+++ javascript.js
@@ -20,9 +20,6 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice().toLowerCase();
-
 function playRound(computerChoice, humanChoice) {
   console.log(`You chose... ${humanChoice}`);
   console.log(`Computer chose... ${computerChoice}`);
@@ -68,4 +65,23 @@ function playRound(computerChoice, humanChoice) {
   }
 }
 
-playRound(computerChoice, humanChoice);
+function playGame() {
+  while (humanScore < 5 && computerScore < 5) {
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice().toLowerCase();
+
+    playRound(computerChoice, humanChoice);
+  }
+
+  console.log(`Computer's score... ${computerScore}`);
+  console.log(`Your score... ${humanScore}`);
+
+  if (computerScore < humanScore) {
+    console.log("You win! Good job!");
+  }
+  else if (computerScore > humanScore) {
+    console.log("You lose! Better luck next time!");
+  }
+}
+
+playGame();
