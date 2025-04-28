eb6f9293ee2ae3a7fcbcd4cdc89b8b57c582356d
markgallant01 <markgallant01@gmail.com>
2025-04-28 00:35:39 -0400

    implement getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+  // return random number in range [0..2]
+  const choice = Math.floor(Math.random() * 3);
+
+  if (choice === 0) {
+    return "rock";
+  }
+  else if (choice === 1) {
+    return "paper";
+  }
+  else if (choice === 2) {
+    return "scissors";
+  }
+}
+
